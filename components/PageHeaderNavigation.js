dc4ce70ce3352543cea08f89f2f6c14defc0f3fb
james-w-balcomb <james.w.balcomb@gmail.com>
2019-10-22 17:32:55 -0400

    remove nav
    remove Menu dropdown
    
    remove Search Box
    
    remove Docs link
    
    remove Options collapse dropdown
    
    remove footer About
    
    remove footer T&C

--- components/PageHeaderNavigation.js
+++ components/PageHeaderNavigation.js
@@ -1,35 +1,10 @@
 import React from 'react'
 import Link from "next/link";
 import {
-    Collapse,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
-    Input,
-    NavbarToggler,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    Nav,
     Navbar,
     NavbarBrand } from "reactstrap";
-import PageHeaderNavigationDropdown from '../components/PageHeaderNavigationDropdown.js'
 
 class PageHeaderNavigation extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpen: false
-        };
-        this.toggle = this.toggle.bind(this);
-    }
-
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
 
     render() {
         return (
@@ -42,45 +17,6 @@ class PageHeaderNavigation extends React.Component {
                             <span className="icon ion-md-home mr-1"/> Little Look-Up
                         </NavbarBrand>
                     </Link>
-                    <Nav navbar>
-                        <PageHeaderNavigationDropdown />
-                    </Nav>
-
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <Input
-                                type="search"
-                                name="search"
-                                id="exampleSearch"
-                                placeholder="search placeholder"
-                            />
-                            <NavItem>
-                                <NavLink href="/docs">Docs</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/tables">Tables</NavLink>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
-                                </DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem>
-                                        Option 1
-                                    </DropdownItem>
-                                    <DropdownItem>
-                                        Option 2
-                                    </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>
-                                        Reset
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                        </Nav>
-                    </Collapse>
-
                 </Navbar>
 
             </React.Fragment>
