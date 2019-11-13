51778af03f61259110f9aaf9ce50f109a02e9746
james-w-balcomb <james.w.balcomb@gmail.com>
2019-11-13 13:08:28 -0500

    clean up presentation, comments, and logging
    also changed contentType in DB to unset for all documents without a contentContent.table

--- pages/content.js
+++ pages/content.js
@@ -10,52 +10,25 @@ const { publicRuntimeConfig } = getConfig();
 const LLU_API_URL = publicRuntimeConfig.LLU_API_URL;
 
 function ContentType({contentDocument}) {
-    console.log("ContentType(contentDocument)");
-
-    console.log(contentDocument);
-
-    console.log(contentDocument.contentType);
+    console.log("ContentPage.ContentType({contentDocument})");
 
     const contentType = contentDocument.contentType;
 
-    console.log(contentType);
-
     if (contentType === "table") {
-        console.log("ContentTypeTable");
+        console.log("contentType: table");
         return <ContentTypeTable  contentDocument={contentDocument} />;
     }
-    console.log("ContentTypeRaw");
+    console.log("contentType: raw");
     return <ContentTypeRaw  contentDocument={contentDocument} />;
-
 }
 
 class ContentPage extends Page {
 
     static async getInitialProps ({ query: { contentPagePath } }) {
-        console.log("ContentPage.getInitialProps()");
-        // console.log("context:");
-        // console.log(context);
-        // console.log("context.asPath:");
-        // console.log(context.asPath);
-        // console.log("context.pathname:");
-        // console.log(context.pathname);
-        // console.log("context.query:");
-        // console.log(context.query);
-        // console.log("query:");
-        // console.log(query);
-
-        // console.log("contentPagePath:");
-        // console.log(contentPagePath);
+        console.log("ContentPage.getInitialProps ({ query: { contentPagePath } })");
 
         const res = await fetch(`${LLU_API_URL}/content/page-path/${contentPagePath}`);
-
-        // console.log("res:");
-        // console.log(res);
-
         const json = await res.json();
-
-        // console.log("json:");
-        // console.log(json);
 
         return { contentDocument: json }
     }
@@ -63,37 +36,13 @@ class ContentPage extends Page {
     render () {
         console.log("ContentPage.render()");
 
-        // console.log("props:");
-        // console.log(props);
-        // console.log("this.props:");
-        // console.log(this.props);
-        // console.log("this.state:");
-        // console.log(this.state);
-
-        // console.log("LLU_API_URL: " + LLU_API_URL);
-
         return (
-            <PageLayout title={this.props.contentDocument.contentPageTitle} description={this.props.contentDocument.contentPageDescription}>
+            <PageLayout
+                title={this.props.contentDocument.contentPageTitle}
+                description={this.props.contentDocument.contentPageDescription}
+            >
 
-                <div>
-                    <h1>
-                        {this.props.contentDocument.contentPageTitle}
-                    </h1>
-                    <h3>
-                        {this.props.contentDocument.contentPagePath}
-                    </h3>
-                    <p>
-                        {this.props.contentDocument.contentPageDescription}
-                    </p>
-                    <p>
-                        this.props.contentDocument.contentContent
-                    </p>
-                </div>
-
-                <hr />
-
-                <ContentType contentDocument={this.props.contentDocument} />,
-
+                <ContentType contentDocument={this.props.contentDocument} />
 
             </PageLayout>
         )
