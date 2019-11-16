5f2bc5c8c382aa61c4c9fe8bfee276be602fcf4c
Seth Warkentien <sethfw@gmail.com>
2019-11-16 11:25:32 -0600

    rollback couple changes

--- next.config.js
+++ next.config.js
@@ -3,20 +3,6 @@ const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
     publicRuntimeConfig: {
-        LLU_API_URL: 'http://localhost:3300',
-    },
-
-    webpack: config => {
-        // Fixes npm packages that depend on `fs` module
-        config.node = {
-            fs: 'empty'
-        };
-
-        config.module.rules.push({
-            test: /\.(png|svg|jpg|gif)$/,
-            use: ["file-loader"]
-        });
-
-        return config
+        LLU_API_URL: process.env.LLU_API_URL,
     }
 })
